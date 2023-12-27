7516553dc6ba47964bcc18e99134f4085a6728ca
aGoodMan999 <21521808@gm.uit.edu.vn>
2023-12-27 13:04:06 +0700

    Update UI màn hình Register

--- course_management_frontend/src/components/Register/RegisterComponents/Step2.tsx
+++ course_management_frontend/src/components/Register/RegisterComponents/Step2.tsx
@@ -1,73 +1,67 @@
-import { FaLongArrowAltRight } from "react-icons/fa"
+import { Tooltip } from "@chakra-ui/react";
+import { useState } from "react";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa"
 
 export const Step2: React.FC<{
-    setStep:Function, 
-    firstName:string, 
-    lastName:string, 
+    setStep: Function,
+    firstName: string,
+    lastName: string,
     major: number,
     setFirstName: Function,
     setLastName: Function,
     setMajor: Function,
+    handleOnClickBack: Function
 }> = (props) => {
 
-    const handleOnFirstNameChanged = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    //FN = FIRST NAME
+    const [isShowFNTooltip, setIsShowFNTooltip] = useState<boolean>(false);
+    //LN = LAST NAME
+    const [isShowLNToolTip, setIsShowLNTooltip] = useState<boolean>(false);
+
+
+    const handleOnFirstNameChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setIsShowFNTooltip(false);
         props.setFirstName(e.target.value);
     }
-    const handleOnLastNameChanged = (e: React.ChangeEvent<HTMLInputElement>) =>{
-        props.setFirstName(e.target.value);
+    const handleOnLastNameChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setIsShowLNTooltip(false);
+        props.setLastName(e.target.value);
     }
-    const handleOnMajorChanged = (e: React.ChangeEvent<HTMLSelectElement>) =>{
+    const handleOnMajorChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const value = +e.target.value;
         console.log(value);
         props.setMajor(value);
-        // switch (value) {
-        //     case 0: {
-        //         props.setMajor('KTPM');
-        //         break;
-        //     }
-        //     case 1: {
-        //         props.setMajor('HTTT');
-        //         break;
-        //     }
-        //     case 2: {
-        //         props.setMajor('KHMT');
-        //         break;
-        //     }
-        //     case 3: {
-        //         props.setMajor('KTMT');
-        //         break;
-        //     }
-        //     case 4: {
-        //         props.setMajor('TMDT');
-        //         break;
-        //     }
-        //     case 5: {
-        //         props.setMajor('KHDL');
-
-        //         break;
-        //     }
-        //     case 6: {
-        //         props.setMajor('MMT');
-        //         break;
-        //     }
-        //     default : return;
-        // }
     }
 
-    const handleOnClickNext = () =>{
-        props.setStep(3);
+    const handleOnClickNext = () => {
+        let canNext = true;
+        if(props.lastName == ''){
+            setIsShowLNTooltip(true);
+            canNext = false;   
+        }
+        if(props.firstName == '') {
+            setIsShowFNTooltip(true);
+            canNext = false;
+        }
+        if(canNext){
+            props.setStep(3);
+        }
     }
 
     return (
-        <div className="w-100 h-100 p-5">
+        <div className="w-100 p-5 pt-0">
             <div className="container">
                 <h5 className="text-center">Hãy điền thêm chút thông tin</h5>
-                <div className="input-group input-group-lg m-4">
+                <div className="input-group input-group-md m-4">
                     <span className="input-group-text">Full name</span>
-                    <input onChange={handleOnFirstNameChanged} value={props.firstName} type="text" placeholder="Enter first name" aria-label="First name" className="form-control" />
-                    <input onChange={handleOnLastNameChanged} value={props.lastName} type="text" placeholder="Enter last name" aria-label="Last name" className="form-control" />
+                    <Tooltip hasArrow placement="bottom-end" isOpen={isShowFNTooltip} label="This field can't be empty" bg='red.600'>
+                        <input onChange={handleOnFirstNameChanged} value={props.firstName} type="text" placeholder="Enter first name" aria-label="First name" className="form-control" />
+                    </Tooltip>
+                    <Tooltip hasArrow placement="bottom-end" isOpen={isShowLNToolTip} label="This field can't be empty" bg='red.600'>
+                        <input onChange={handleOnLastNameChanged} value={props.lastName} type="text" placeholder="Enter last name" aria-label="Last name" className="form-control" />
+                    </Tooltip>
                 </div>
-                <div className="input-group input-group-lg m-4">
+                <div className="input-group input-group-md m-4">
                     <label className="input-group-text" htmlFor="inputGroupSelect01">Major </label>
                     <select onChange={handleOnMajorChanged} className="form-select" id="inputGroupSelect01" value={props.major}>
                         <optgroup label="Chose your major">
@@ -82,8 +76,9 @@ export const Step2: React.FC<{
                     </select>
                 </div>
             </div>
-            <div className="d-flex justify-content-center">
-                <button onClick={handleOnClickNext} className="btn btn-primary w-75 h-25">NEXT     <FaLongArrowAltRight size={30} className="d-inline-block" /></button>
+            <div className="row justify-content-around">
+                <button onClick={() => (props.handleOnClickBack())} className="btn btn-secondary h-25 col-5"><FaLongArrowAltLeft size={30} className="d-inline-block" />&nbsp;BACK</button>
+                <button onClick={handleOnClickNext} className="btn btn-primary h-25 col-5">NEXT&nbsp;<FaLongArrowAltRight size={30} className="d-inline-block" /></button>
             </div>
         </div>
     )
--- course_management_frontend/src/components/Register/RegisterComponents/Step3.tsx
+++ course_management_frontend/src/components/Register/RegisterComponents/Step3.tsx
@@ -3,9 +3,15 @@ import { CreditItem } from "./CreditItem"
 import { Type } from "../../../model/TypeAndTotal"
 import style from "../styles/Step3.module.css"
 import { Target } from "../../../model/Target"
+import { FaLongArrowAltLeft } from "react-icons/fa"
 
 
-export const Step3: React.FC<{ creditTypeList: Target[], handleOnFinish: Function, handleOnChangeTotal:Function }> = (props) => {
+export const Step3: React.FC<{
+    creditTypeList: Target[],
+    handleOnFinish: Function,
+    handleOnChangeTotal: Function
+    handleOnClickBack: Function
+}> = (props) => {
 
 
 
@@ -13,7 +19,7 @@ export const Step3: React.FC<{ creditTypeList: Target[], handleOnFinish: Functio
 
 
     return (
-        <div className="w-100 p-5">
+        <div className="w-100 p-5 pt-0 pb-0">
             <div className="container">
                 <h5 className="text-center">Sắp xong rồi ! Điền thông tin tín chỉ mục tiêu của bạn để bắt đầu trải nghiệm</h5>
                 <div className="container w-75">
@@ -26,8 +32,9 @@ export const Step3: React.FC<{ creditTypeList: Target[], handleOnFinish: Functio
                         })}
                     </ul>
                 </div>
-                <div className="d-flex justify-content-center mt-1">
-                    <button onClick={()=>{props.handleOnFinish()}} className="btn btn-primary w-75 h-25">FINISH</button>
+                <div className="row justify-content-around mt-3">
+                    <button onClick={() => (props.handleOnClickBack())} className="btn btn-secondary h-25 col-5"><FaLongArrowAltLeft size={30} className="d-inline-block" />&nbsp;BACK</button>
+                    <button onClick={() => { props.handleOnFinish() }} className="btn btn-primary col-5 h-25">FINISH</button>
                 </div>
             </div>
         </div>
